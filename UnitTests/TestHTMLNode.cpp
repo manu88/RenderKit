3a59016bc75030da51b9997bdb4986a7aeb6de4d
manu88 <manuel.deneu@gmail.com>
2017-06-14 12:55:55 +0200

    Renamed HTMLTree unit to HTMLNode + new unit test

---
+++ UnitTests/TestHTMLNode.cpp
@@ -0,0 +1,9 @@
+//
+//  TestHTMLNode.cpp
+//  RenderKit
+//
+//  Created by Manuel Deneu on 14/06/2017.
+//  Copyright © 2017 Unlimited Development. All rights reserved.
+//
+
+#include "TestHTMLNode.hpp"
---
+++ UnitTests/TestHTMLNode.hpp
@@ -0,0 +1,14 @@
+//
+//  TestHTMLNode.hpp
+//  RenderKit
+//
+//  Created by Manuel Deneu on 14/06/2017.
+//  Copyright © 2017 Unlimited Development. All rights reserved.
+//
+
+#ifndef TestHTMLNode_hpp
+#define TestHTMLNode_hpp
+
+#include <stdio.h>
+
+#endif /* TestHTMLNode_hpp */
